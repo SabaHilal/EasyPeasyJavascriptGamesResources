6255239374b0fff5fe53bba1dbd1c07e6c3a8b6a
SabaHilal <57343679+SabaHilal@users.noreply.github.com>
2023-11-21 12:16:16 +0530

    Add files via upload

---
+++ TheRainOfColors/Drop.js
@@ -0,0 +1,33 @@
+class Drop {
+    constructor(x,y,h){
+        var options = {
+            speed : 30,
+            density :1.0
+        }
+        this.body = Bodies.rectangle(x,y,4,h, options);
+        this.h = h;
+        World.add(world, this.body);
+        }
+    
+
+        display(){
+        var pos = this.body.position;
+        push();
+        translate(pos.x,pos.y);
+        
+        var b = random (20,255);
+        var g = random (20,255);
+        var r = random (20,255);
+        stroke(b,g,r);
+        fill(b,g,r);
+        rect(0,0,1,this.h);
+        pop();
+        }
+
+
+        isOffScreen(){
+        var pos = this.body.position;
+        return (pos.y>600);
+        }
+    
+    }
---
+++ TheRainOfColors/sketch.js
@@ -0,0 +1,39 @@
+const { Engine, World, Bodies } = Matter ;
+var drop
+var rain = [];
+var x,y,w,h = 0;
+var backgroundImg;
+
+  function preload(){
+    backgroundImg = loadImage("sky.png"); 
+  }
+  function setup() {
+      engine = Engine.create();
+      world = engine.world;
+      var canvas = createCanvas(1200,600);
+           
+  }
+
+  function newDrop() {
+      for (var i=0; i<20; i++){
+        x = random (0,1200);
+        y = random(-20,100);
+        h = random (3,7);
+        rain.push(new Drop(x,y,h));
+      }
+  }
+
+  function draw() {
+      background(backgroundImg);  
+      Engine.update(engine);
+     newDrop();
+     for (var j = 0; j < rain.length ; j++) {
+     rain[j].display();
+
+      if (rain[j].isOffScreen()){
+       rain.splice(j,1);
+      }
+  
+    } 
+
+  }
